5fc4a57aac2274f4477c751a935f02b5197a6853
20100 <mmmelkady23@gmail.com>
2024-12-19 00:43:42 +0200

    Answer 13

--- Program.cs
+++ Program.cs
@@ -1,10 +1,21 @@
-﻿namespace Assingment_Three_Basic
+﻿using System.Text;
+
+namespace Assingment_Three_Basic
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q13- Write a program to allow the user to enter a string and print the REVERSE of it.
+            Console.WriteLine("please Enter string : ");
+            string stri = Console.ReadLine();
+            StringBuilder sb = new StringBuilder();
+            for (int i = stri.Length-1; i >=0; i--)
+            {
+                sb.Append(stri[i]);
+            }
+            Console.WriteLine(sb.ToString());
+            #endregion
         }
     }
 }
