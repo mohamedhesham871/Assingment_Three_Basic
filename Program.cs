1cdb835f1856753b7a883e5edddda9b3101ee9e9
20100 <mmmelkady23@gmail.com>
2024-12-19 02:14:14 +0200

    answer 17

--- Program.cs
+++ Program.cs
@@ -51,26 +51,55 @@ namespace Assingment_Three_Basic
             #endregion
 
             #region Q16:Write a program in C# Sharp to convert a decimal number into binary without using an array
-            Console.WriteLine("enter number to convert it to Binary ");
-            int x=int.Parse(Console.ReadLine());
-            StringBuilder sb = new StringBuilder();
-            while (x>0)
-            {
-                
-                 if (x / 2.0 == x / 2)
-                {
-                    sb.Insert(0, 0);
-                }
-                else
-                {
-                    sb.Insert(0, 1);
-                }
-                x /= 2;
-             
+            //Console.WriteLine("enter number to convert it to Binary ");
+            //int x=int.Parse(Console.ReadLine());
+            //StringBuilder sb = new StringBuilder();
+            //while (x>0)
+            //{
 
+            //     if (x / 2.0 == x / 2)
+            //    {
+            //        sb.Insert(0, 0);
+            //    }
+            //    else
+            //    {
+            //        sb.Insert(0, 1);
+            //    }
+            //    x /= 2;
+
+
+            //}
+            //int Binrynumber =int.Parse(sb.ToString());
+            //Console.WriteLine($"The Binary of {x} is {Binrynumber}");
+            #endregion
+
+            #region Q17:Create a program that asks the user to input three points (x1, y1), (x2, y2), and (x3, y3), and determines whether these points lie on a single straight line.
+            Console.WriteLine("enter x and y  of point1 : ");
+            string point1 = Console.ReadLine();
+            double pointX1 = double.Parse(point1.Split(' ')[0]);
+            double pointY1 = double.Parse(point1.Split(' ')[1]);
+
+            Console.WriteLine("enter x and y of point2 : ");
+            string point2 = Console.ReadLine();
+            double pointX2 = double.Parse(point2.Split(' ')[0]);
+            double pointY2 = double.Parse(point2.Split(' ')[1]);
+
+            Console.WriteLine("enter xand y of point3 : ");
+            string point3 = Console.ReadLine();
+            double pointX3 = double.Parse(point3.Split(' ')[0]);
+            double pointY3 = double.Parse(point3.Split(' ')[1]);
+
+            // calculate if they on same line
+            double area = 0.5 * Math.Abs(pointX1 * (pointY2 - pointY3) + pointX2 * (pointY3 - pointY1) + pointX3 * (pointY1 - pointY2));
+            if(area==0)
+            {
+                Console.WriteLine("they are on same Line");
             }
-            int Binrynumber =int.Parse(sb.ToString());
-            Console.WriteLine($"The Binary of {x} is {Binrynumber}");
+            else
+            {
+                Console.WriteLine("they not on same Line");
+            }
+
             #endregion
         }
     }
