5a22ce85bdbee9ce48c9a5277373017e35c88c43
20100 <mmmelkady23@gmail.com>
2024-12-19 02:32:14 +0200

    Last one

--- Program.cs
+++ Program.cs
@@ -74,32 +74,66 @@ namespace Assingment_Three_Basic
             #endregion
 
             #region Q17:Create a program that asks the user to input three points (x1, y1), (x2, y2), and (x3, y3), and determines whether these points lie on a single straight line.
-            Console.WriteLine("enter x and y  of point1 : ");
-            string point1 = Console.ReadLine();
-            double pointX1 = double.Parse(point1.Split(' ')[0]);
-            double pointY1 = double.Parse(point1.Split(' ')[1]);
+            //Console.WriteLine("enter x and y  of point1 : ");
+            //string point1 = Console.ReadLine();
+            //double pointX1 = double.Parse(point1.Split(' ')[0]);
+            //double pointY1 = double.Parse(point1.Split(' ')[1]);
 
-            Console.WriteLine("enter x and y of point2 : ");
-            string point2 = Console.ReadLine();
-            double pointX2 = double.Parse(point2.Split(' ')[0]);
-            double pointY2 = double.Parse(point2.Split(' ')[1]);
+            //Console.WriteLine("enter x and y of point2 : ");
+            //string point2 = Console.ReadLine();
+            //double pointX2 = double.Parse(point2.Split(' ')[0]);
+            //double pointY2 = double.Parse(point2.Split(' ')[1]);
 
-            Console.WriteLine("enter xand y of point3 : ");
-            string point3 = Console.ReadLine();
-            double pointX3 = double.Parse(point3.Split(' ')[0]);
-            double pointY3 = double.Parse(point3.Split(' ')[1]);
+            //Console.WriteLine("enter xand y of point3 : ");
+            //string point3 = Console.ReadLine();
+            //double pointX3 = double.Parse(point3.Split(' ')[0]);
+            //double pointY3 = double.Parse(point3.Split(' ')[1]);
 
-            // calculate if they on same line
-            double area = 0.5 * Math.Abs(pointX1 * (pointY2 - pointY3) + pointX2 * (pointY3 - pointY1) + pointX3 * (pointY1 - pointY2));
-            if(area==0)
+            //// calculate if they on same line
+            //double area = 0.5 * Math.Abs(pointX1 * (pointY2 - pointY3) + pointX2 * (pointY3 - pointY1) + pointX3 * (pointY1 - pointY2));
+            //if(area==0)
+            //{
+            //    Console.WriteLine("they are on same Line");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("they not on same Line");
+            //}
+
+            #endregion
+
+            #region Q18:
+            /* 
+             Within a company, the efficiency of workers is evaluated based on the duration required to complete a specific task.
+             A worker's efficiency level is determined as follows: 
+             -If the worker completes the job within 2 to 3 hours, they are considered highly efficient.
+             - If the worker takes 3 to 4 hours, they are instructed to increase their speed.
+             - If the worker takes 4 to 5 hours, they are provided with training to enhance their speed.
+             - If the worker takes more than 5 hours, they are required to leave the company.
+              To calculate the efficiency of a worker, the time taken for the task is obtained via user input from the keyboard
+             */
+            Console.WriteLine("enter input ");
+            double hours =double.Parse(Console.ReadLine());
+            string message;
+            if(hours >=2 && hours<3)
             {
-                Console.WriteLine("they are on same Line");
+                message = "highly efficient";
             }
-            else
+            else if(hours >= 3 && hours < 4)
             {
-                Console.WriteLine("they not on same Line");
+                message = "instructed to increase their speed";
+            }
+            else if(hours >= 4 && hours < 5)
+            {
+                message = "they are provided with training to enhance their speed";
+            }
+            else if(hours >= 5 )
+            {
+                message = "they are required to leave the company";
             }
 
+
+        
             #endregion
         }
     }
